f5ad0b86a74ef0bc0eed1b14590cd8951f2daf6f
shan-desh <shandesh19@gmail.com>
2022-05-27 09:02:45 +0530

    Simple flask application

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,9 @@
+from flask import Flask
 
-print("Hello World !")
+app = Flask(__name__)
+
+@app.route('/')
+def message():
+    return 'Hello World from Python Flask!'
+
+app.run(host='0.0.0.0', port=8081)
